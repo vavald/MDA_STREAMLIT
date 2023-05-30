1bcaf565eac04fcf6aed9b5018d5de1ba3265542
--replace-all <vanpoeyer.silke@gmail.com>
2023-05-30 16:19:33 +0200

    Adaptation Weather-Telraam: title & small cleanup

--- pages/Weather_Telraam.py
+++ pages/Weather_Telraam.py
@@ -34,104 +34,49 @@ print(model.info())
 
 # In[ ]:
 
-
 model.day_week.unique()
 
-
 # In[ ]:
-
 
 model.month.unique()
 
-
 # In[ ]:
-
-
 # get month names from number names
 
 import calendar
 
-
 # In[ ]:
-
-
 
 model['month'] = model['month'].apply(lambda x: calendar.month_name[x])
 
-
 # In[ ]:
-
 
 model.month.unique()
 
-
 # In[ ]:
-
-
 
 model.day_week.unique()
 
-
 # In[ ]:
-
 
 model['day_week'] = model['day_week'].apply(lambda x: calendar.day_name[x-1])
 
-
 # In[ ]:
-
 
 model.day_week.unique()
 
-
 # ### Plots  data
-
 # In[ ]:
-
 
 import plotly.express as px
 
+# In[ ]:
+
+st.title('Insights in the Model Dataset')
 
 # In[ ]:
 
-
-
-# |exporti
-
-st.markdown(
-    """
-    The scatter plot 'Noise level of average cars per day in one month' shows us the noise level produced by
-    the average cars per day. 
-    The colors represent the noise levels with its correspondent value. 
-    Higher level of noise tends to have a yellow color.
-    
-    For example, in the 14th day we can see a noise level of 73.56 dB with a averge of autos of 80. 
-    However it is important to note that higher average of autos does not imply higher noise level 
-    We can see for example in the 14th day that a noise level of  80,20dB is obtained when the average autos is 41,69.
-    Also in the day 27 we can find two picks at the same day with a different number of average autos:
-     Noise level of 81 dB when the average autos is 30 and again, noise level of 81,2db when the average autos is 54,47
-     
-     Therefore, different average autos can offer the same noise level ( in different periods)
-"""
-)
-
-
-# In[ ]:
-
-
-
-fig = px.scatter(model, x="day_month", y="avg_cars", animation_frame="day_month", animation_group="lcpeak_avg",
-           color="lcpeak_avg", hover_name="lcpeak_avg",
-           size_max=55, range_x=[-2,33], range_y=[-2,100],
-          title=" Noise level of average cars per day in one month")
-st.plotly_chart(fig)
-
-
-# In[ ]:
-
-
-
-# |exporti
+st.header('Model - Weather dataset')
 
 st.markdown(
     """
@@ -145,16 +90,11 @@ st.markdown(
    In the second semester, although the sun radiation decreases, the noise levels do not follow the same behavior.
    
    December has lower sun radiation than january, however the noise levels are higher( because of Christmas Eve and other 
-   holidays)
-   
-
-   
-"""
+   holidays)  """
 )
 
 
 # In[ ]:
-
 
 fig = px.scatter(model, x="day_month", y="LC_RAD60", animation_frame="month", animation_group="lcpeak_avg",
            color="lcpeak_avg", hover_name="lcpeak_avg",
@@ -162,42 +102,25 @@ fig = px.scatter(model, x="day_month", y="LC_RAD60", animation_frame="month", an
           title=" Sun radiation and noise level per day in one year")
 st.plotly_chart(fig)
 
-
 # In[ ]:
-
-
-
-
-# |exporti
 
 st.markdown(
     """
    The scatter plot shows us the relationship between Temperature and Noise level per day in one year, 
-   
    The colors respresent the noise levels whit its correspondent value. 
-   
    Higher level of noise tends to have a yellow color.
-   
    In January and February, the noise level was not high and the noise level too. 
-   
    In March there is a difference respect to the first two months of the year, the temperature increases and the noise level
    too. This tendecy prevails until June. 
-   
    Despite Juli has higher temperatures, the noise level decreases (People is on vacantions)
-   
    Although the temperature decreases after July, we cannot assume that the noise level decreases directly.
    We see the tendency that the noise level does not change significantly. 
-   Noise levels do not excede 80dB (only in a counted number of days).
-   
-   
-    
+   Noise levels do not excede 80dB (only in a counted number of days). 
 """
 )
 
 
 # In[ ]:
-
-
 
 fig = px.scatter(model, x="day_month", y="LC_TEMP", animation_frame="month", animation_group="lcpeak_avg",
          color="lcpeak_avg", hover_name="lcpeak_avg",range_y=[-15,45],
@@ -206,10 +129,6 @@ st.plotly_chart(fig)
 
 
 # In[ ]:
-
-
-
-# |exporti
 
 st.markdown(
     """
@@ -227,15 +146,11 @@ st.markdown(
     Monday to Friday: between 6:40 am and 12:10 pm, the sun radiation increases and the noise too.
                      from 12:12 pm to 4:00 pm , sun radiation increases but noise level not.
                      After 4:pm sun radiation decreases and noise level too.
-                   
-    
-    
 """
 )
 
 
 # In[ ]:
-
 
 # add column hour:minute to noise data
 model['10_min_interval_start_time'] = model['hour'].astype(str) + ':' + model['minute'].astype(str)
@@ -243,20 +158,13 @@ model['10_min_interval_start_time'] = model['hour'].astype(str) + ':' + model['m
 
 # In[ ]:
 
-
-
 fig = px.scatter(model, x="day_week", y="LC_RAD60", animation_frame="10_min_interval_start_time", animation_group="lcpeak_avg",
            color="lcpeak_avg", hover_name="lcpeak_avg",
            size_max=55, range_x=[-0.5,7.5], range_y=[-45,871],
           title=" Sun radiation vs Noise level per day every 10 minutes")
 st.plotly_chart(fig)
 
-
 # In[ ]:
-
-
-
-# |exporti
 
 st.markdown(
     """
@@ -269,17 +177,11 @@ st.markdown(
    However this tendency changes between 12:10 and 12:40 pm, the noise level increases regarless temperature and sun radiation.
    
    After 16:40 hr, as the sun radiation decreases, the noise level also decrease (regardless temperature) 
-    
-    
-    
-    
 """
 )
 
 
 # In[ ]:
-
-
 
 fig = px.scatter(model, x="LC_TEMP", y="LC_RAD60", animation_frame="10_min_interval_start_time", animation_group="lcpeak_avg",
            color="lcpeak_avg", hover_name="lcpeak_avg",
@@ -288,12 +190,34 @@ fig = px.scatter(model, x="LC_TEMP", y="LC_RAD60", animation_frame="10_min_inter
 
 st.plotly_chart(fig)
 
+# In[ ]:
+
+st.header('Model - Telraam dataset')
+
+st.markdown(
+    """
+    The scatter plot 'Noise level of average cars per day in one month' shows us the noise level produced by
+    the average cars per day. 
+    The colors represent the noise levels with its correspondent value. 
+    Higher level of noise tends to have a yellow color.
+    
+    For example, in the 14th day we can see a noise level of 73.56 dB with a averge of autos of 80. 
+    However it is important to note that higher average of autos does not imply higher noise level 
+    We can see for example in the 14th day that a noise level of  80,20dB is obtained when the average autos is 41,69.
+    Also in the day 27 we can find two picks at the same day with a different number of average autos:
+    Noise level of 81 dB when the average autos is 30 and again, noise level of 81,2db when the average autos is 54,47
+     
+     Therefore, different average autos can offer the same noise level ( in different periods)
+"""
+)
 
 # In[ ]:
 
-
-
-# |exporti
+fig = px.scatter(model, x="day_month", y="avg_cars", animation_frame="day_month", animation_group="lcpeak_avg",
+           color="lcpeak_avg", hover_name="lcpeak_avg",
+           size_max=55, range_x=[-2,33], range_y=[-2,100],
+          title=" Noise level of average cars per day in one month")
+st.plotly_chart(fig)
 
 st.markdown(
     """
@@ -327,32 +251,14 @@ st.markdown(
    
    However, Friday, Saturday and Sunday at 7:10 pm, we can see that the noise level starts to increase and the 
    average autos is smaller than the afternoon time in the whole week.
-     
-    
-    
-    
-
-    
-    
-    
-    
 """
 )
 
 
 # In[ ]:
 
-
-
 fig = px.scatter(model, x="day_week", y="avg_cars", animation_frame="10_min_interval_start_time", animation_group="lcpeak_avg",
            color="lcpeak_avg", hover_name="lcpeak_avg",
            size_max=55, range_x=[-0.5,8], range_y=[-2,100],
           title=" Noise level of average cars per day every 10 minutes")
 st.plotly_chart(fig)
-
-
-# In[ ]:
-
-
-
-
